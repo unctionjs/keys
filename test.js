b954f071375aa58cbad5a7b193b6a73c5acc5f5f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:52 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import keys from "./"
+import keys from "./index"
 
 test(({same, end}) => {
   same(
